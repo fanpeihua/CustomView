369742c09bacc71c6a35ce38e21d219f68540208
LWNfph19940924 <fph19930825@163.com>
2018-05-06 22:41:02 +0800

    Initial commit

---
+++ app/src/main/java/fanpeihua/customview/boheruler/innerRulers/InnerRuler.java
@@ -0,0 +1,21 @@
+package fanpeihua.customview.boheruler.innerRulers;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class InnerRuler extends View{
+    public InnerRuler(Context context) {
+        super(context);
+    }
+
+    public InnerRuler(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public InnerRuler(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
+
